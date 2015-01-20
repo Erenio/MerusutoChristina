0c6fcfc0f964b2f74288e0cad385a2fef1e65e26
Theo <bbtfrr@gmail.com>
2015-01-20 21:41:36 +0800

    Automated commit at 2015-01-20 13:41:36 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -5608,71 +5608,71 @@ window.$ === undefined && (window.$ = Zepto)
       (function() {
         __out.push(_.renderTemplate("templates/modals/header"));
       
-        __out.push('\n<div class="wrap">\n  <img class="image" src="');
+        __out.push('\n<div class="content">\n  <div class="slider">\n    <div class="slide-group">\n      <div class="slide">\n        <img class="image" src="');
       
         __out.push(__sanitize(this.model.originalUrl()));
       
-        __out.push('">\n\n  <div class="media">\n    <div class="media-body">\n      <div class="media-info-group">\n        <p class="media-info">\n          生命：<span id="life">');
+        __out.push('">\n      </div>\n      <div class="slide media">\n        <div class="media-body">\n          <div class="media-info-group">\n            <p class="media-info">\n              生命：<span id="life">');
       
         __out.push(__sanitize(this.model.get("life")));
       
-        __out.push('</span><br>\n          攻击：<span id="atk">');
+        __out.push('</span><br>\n              攻击：<span id="atk">');
       
         __out.push(__sanitize(this.model.get("atk")));
       
-        __out.push('</span><br>\n          攻距：');
+        __out.push('</span><br>\n              攻距：');
       
         __out.push(__sanitize(this.model.get("aarea")));
       
-        __out.push('<br>\n          攻数：');
+        __out.push('<br>\n              攻数：');
       
         __out.push(__sanitize(this.model.get("anum")));
       
-        __out.push('<br>\n          攻速：');
+        __out.push('<br>\n              攻速：');
       
         __out.push(__sanitize(this.model.get("aspd")));
       
-        __out.push('<br>\n          韧性：');
+        __out.push('<br>\n              韧性：');
       
         __out.push(__sanitize(this.model.get("tenacity")));
       
-        __out.push('<br>\n          移速：');
+        __out.push('<br>\n              移速：');
       
         __out.push(__sanitize(this.model.get("mspd")));
       
-        __out.push('<br>\n          成长：');
+        __out.push('<br>\n              成长：');
       
         __out.push(__sanitize(this.model.getTypeString()));
       
-        __out.push('<br>\n        </p>\n        <p class="media-info">\n          火：');
+        __out.push('<br>\n            </p>\n            <p class="media-info">\n              火：');
       
         __out.push(__sanitize(Math.round(this.model.get("fire") * 100)));
       
-        __out.push('%<br>\n          水：');
+        __out.push('%<br>\n              水：');
       
         __out.push(__sanitize(Math.round(this.model.get("aqua") * 100)));
       
-        __out.push('%<br>\n          风：');
+        __out.push('%<br>\n              风：');
       
         __out.push(__sanitize(Math.round(this.model.get("wind") * 100)));
       
-        __out.push('%<br>\n          光：');
+        __out.push('%<br>\n              光：');
       
         __out.push(__sanitize(Math.round(this.model.get("light") * 100)));
       
-        __out.push('%<br>\n          暗：');
+        __out.push('%<br>\n              暗：');
       
         __out.push(__sanitize(Math.round(this.model.get("dark") * 100)));
       
-        __out.push('%<br><br>\n        </p>\n        <p class="media-info">\n          DPS：');
+        __out.push('%<br><br>\n            </p>\n            <p class="media-info">\n              DPS：');
       
         __out.push(__sanitize(Math.round(this.model.get("dps"))));
       
-        __out.push('<br>\n          MDPS：');
+        __out.push('<br>\n              MDPS：');
       
         __out.push(__sanitize(Math.round(this.model.get("mdps"))));
       
-        __out.push('<br>\n        </p>\n      </div>\n    </div>\n  </div>\n</div>\n');
+        __out.push('<br>\n            </p>\n          </div>\n        </div>\n      </div>\n    </div>\n  </div>\n</div>\n');
       
       }).call(this);
       
@@ -6265,6 +6265,203 @@ window.$ === undefined && (window.$ = Zepto)
   })(Backbone.View);
 
 }).call(this);
+/* ========================================================================
+ * Ratchet: sliders.js v2.0.2
+ * http://goratchet.com/components#sliders
+ * ========================================================================
+   Adapted from Brad Birdsall's swipe
+ * Copyright 2014 Connor Sears
+ * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
+ * ======================================================================== */
+
+
+!(function () {
+  'use strict';
+
+  var pageX;
+  var pageY;
+  var slider;
+  var deltaX;
+  var deltaY;
+  var offsetX;
+  var lastSlide;
+  var startTime;
+  var resistance;
+  var sliderWidth;
+  var slideNumber;
+  var isScrolling;
+  var scrollableArea;
+
+  var getSlider = function (target) {
+    var i;
+    var sliders = document.querySelectorAll('.slider > .slide-group');
+
+    for (; target && target !== document; target = target.parentNode) {
+      for (i = sliders.length; i--;) {
+        if (sliders[i] === target) {
+          return target;
+        }
+      }
+    }
+  };
+
+  var getScroll = function () {
+    if ('webkitTransform' in slider.style) {
+      var translate3d = slider.style.webkitTransform.match(/translate3d\(([^,]*)/);
+      var ret = translate3d ? translate3d[1] : 0;
+      return parseInt(ret, 10);
+    }
+  };
+
+  var setSlideNumber = function (offset) {
+    var round = offset ? (deltaX < 0 ? 'ceil' : 'floor') : 'round';
+    slideNumber = Math[round](getScroll() / (scrollableArea / slider.children.length));
+    slideNumber += offset;
+    slideNumber = Math.min(slideNumber, 0);
+    slideNumber = Math.max(-(slider.children.length - 1), slideNumber);
+  };
+
+  var onTouchStart = function (e) {
+    slider = getSlider(e.target);
+
+    if (!slider) {
+      return;
+    }
+
+    var firstItem  = slider.querySelector('.slide');
+
+    scrollableArea = firstItem.offsetWidth * slider.children.length;
+    isScrolling    = undefined;
+    sliderWidth    = slider.offsetWidth;
+    resistance     = 1;
+    lastSlide      = -(slider.children.length - 1);
+    startTime      = +new Date();
+    pageX          = e.touches[0].pageX;
+    pageY          = e.touches[0].pageY;
+    deltaX         = 0;
+    deltaY         = 0;
+
+    setSlideNumber(0);
+
+    slider.style['-webkit-transition-duration'] = 0;
+  };
+
+  var onTouchMove = function (e) {
+    if (e.touches.length > 1 || !slider) {
+      return; // Exit if a pinch || no slider
+    }
+
+    deltaX = e.touches[0].pageX - pageX;
+    deltaY = e.touches[0].pageY - pageY;
+    pageX  = e.touches[0].pageX;
+    pageY  = e.touches[0].pageY;
+
+    if (typeof isScrolling === 'undefined') {
+      isScrolling = Math.abs(deltaY) > Math.abs(deltaX);
+    }
+
+    if (isScrolling) {
+      return;
+    }
+
+    offsetX = (deltaX / resistance) + getScroll();
+
+    e.preventDefault();
+
+    resistance = slideNumber === 0         && deltaX > 0 ? (pageX / sliderWidth) + 1.25 :
+                 slideNumber === lastSlide && deltaX < 0 ? (Math.abs(pageX) / sliderWidth) + 1.25 : 1;
+
+    slider.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
+  };
+
+  var onTouchEnd = function (e) {
+    if (!slider || isScrolling) {
+      return;
+    }
+
+    setSlideNumber(
+      (+new Date()) - startTime < 1000 && Math.abs(deltaX) > 15 ? (deltaX < 0 ? -1 : 1) : 0
+    );
+
+    offsetX = slideNumber * sliderWidth;
+
+    slider.style['-webkit-transition-duration'] = '.2s';
+    slider.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
+
+    e = new CustomEvent('slide', {
+      detail: { slideNumber: Math.abs(slideNumber) },
+      bubbles: true,
+      cancelable: true
+    });
+
+    slider.parentNode.dispatchEvent(e);
+  };
+
+  window.Ratchet.Sliders = {
+    onTouchStart: onTouchStart,
+    onTouchMove: onTouchMove,
+    onTouchEnd: onTouchEnd
+  };
+
+}());
+(function() {
+  var __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  App.Widgets.Slider = (function(_super) {
+    __extends(Slider, _super);
+
+    function Slider() {
+      return Slider.__super__.constructor.apply(this, arguments);
+    }
+
+    Slider.prototype.events = {
+      "touchstart": "onTouchStart",
+      "touchmove": "onTouchMove",
+      "touchend": "onTouchEnd",
+      "mousedown": "onMouseDown",
+      "mousemove": "onMouseMove",
+      "mouseup": "onMouseUp"
+    };
+
+    Slider.prototype.onTouchStart = function(event) {
+      return Ratchet.Sliders.onTouchStart(event);
+    };
+
+    Slider.prototype.onTouchMove = function(event) {
+      return Ratchet.Sliders.onTouchMove(event);
+    };
+
+    Slider.prototype.onTouchEnd = function(event) {
+      return Ratchet.Sliders.onTouchEnd(event);
+    };
+
+    Slider.prototype._imitateTouchEvent = function(event) {
+      event.touches || (event.touches = [_.pick(event, "pageX", "pageY")]);
+      return event;
+    };
+
+    Slider.prototype.onMouseDown = function(event) {
+      this.mouseIsDown = true;
+      return this.onTouchStart(this._imitateTouchEvent(event));
+    };
+
+    Slider.prototype.onMouseMove = function(event) {
+      if (this.mouseIsDown) {
+        return this.onTouchMove(this._imitateTouchEvent(event));
+      }
+    };
+
+    Slider.prototype.onMouseUp = function(event) {
+      this.mouseIsDown = false;
+      return this.onTouchEnd(this._imitateTouchEvent(event));
+    };
+
+    return Slider;
+
+  })(Backbone.View);
+
+}).call(this);
 (function() {
   var __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
@@ -6278,12 +6475,8 @@ window.$ === undefined && (window.$ = Zepto)
 
     CompanionsShow.prototype.template = _.loadTemplate("templates/modals/companions/show");
 
-    CompanionsShow.prototype.events = {
-      "click": "closeModal"
-    };
-
-    CompanionsShow.prototype.closeModal = function() {
-      return Backbone.history.loadUrl("#close-modal");
+    CompanionsShow.prototype.layout = {
+      ".slider": App.Widgets.Slider
     };
 
     return CompanionsShow;
@@ -6482,203 +6675,6 @@ window.$ === undefined && (window.$ = Zepto)
     };
 
     return Main;
-
-  })(Backbone.View);
-
-}).call(this);
-/* ========================================================================
- * Ratchet: sliders.js v2.0.2
- * http://goratchet.com/components#sliders
- * ========================================================================
-   Adapted from Brad Birdsall's swipe
- * Copyright 2014 Connor Sears
- * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
- * ======================================================================== */
-
-
-!(function () {
-  'use strict';
-
-  var pageX;
-  var pageY;
-  var slider;
-  var deltaX;
-  var deltaY;
-  var offsetX;
-  var lastSlide;
-  var startTime;
-  var resistance;
-  var sliderWidth;
-  var slideNumber;
-  var isScrolling;
-  var scrollableArea;
-
-  var getSlider = function (target) {
-    var i;
-    var sliders = document.querySelectorAll('.slider > .slide-group');
-
-    for (; target && target !== document; target = target.parentNode) {
-      for (i = sliders.length; i--;) {
-        if (sliders[i] === target) {
-          return target;
-        }
-      }
-    }
-  };
-
-  var getScroll = function () {
-    if ('webkitTransform' in slider.style) {
-      var translate3d = slider.style.webkitTransform.match(/translate3d\(([^,]*)/);
-      var ret = translate3d ? translate3d[1] : 0;
-      return parseInt(ret, 10);
-    }
-  };
-
-  var setSlideNumber = function (offset) {
-    var round = offset ? (deltaX < 0 ? 'ceil' : 'floor') : 'round';
-    slideNumber = Math[round](getScroll() / (scrollableArea / slider.children.length));
-    slideNumber += offset;
-    slideNumber = Math.min(slideNumber, 0);
-    slideNumber = Math.max(-(slider.children.length - 1), slideNumber);
-  };
-
-  var onTouchStart = function (e) {
-    slider = getSlider(e.target);
-
-    if (!slider) {
-      return;
-    }
-
-    var firstItem  = slider.querySelector('.slide');
-
-    scrollableArea = firstItem.offsetWidth * slider.children.length;
-    isScrolling    = undefined;
-    sliderWidth    = slider.offsetWidth;
-    resistance     = 1;
-    lastSlide      = -(slider.children.length - 1);
-    startTime      = +new Date();
-    pageX          = e.touches[0].pageX;
-    pageY          = e.touches[0].pageY;
-    deltaX         = 0;
-    deltaY         = 0;
-
-    setSlideNumber(0);
-
-    slider.style['-webkit-transition-duration'] = 0;
-  };
-
-  var onTouchMove = function (e) {
-    if (e.touches.length > 1 || !slider) {
-      return; // Exit if a pinch || no slider
-    }
-
-    deltaX = e.touches[0].pageX - pageX;
-    deltaY = e.touches[0].pageY - pageY;
-    pageX  = e.touches[0].pageX;
-    pageY  = e.touches[0].pageY;
-
-    if (typeof isScrolling === 'undefined') {
-      isScrolling = Math.abs(deltaY) > Math.abs(deltaX);
-    }
-
-    if (isScrolling) {
-      return;
-    }
-
-    offsetX = (deltaX / resistance) + getScroll();
-
-    e.preventDefault();
-
-    resistance = slideNumber === 0         && deltaX > 0 ? (pageX / sliderWidth) + 1.25 :
-                 slideNumber === lastSlide && deltaX < 0 ? (Math.abs(pageX) / sliderWidth) + 1.25 : 1;
-
-    slider.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
-  };
-
-  var onTouchEnd = function (e) {
-    if (!slider || isScrolling) {
-      return;
-    }
-
-    setSlideNumber(
-      (+new Date()) - startTime < 1000 && Math.abs(deltaX) > 15 ? (deltaX < 0 ? -1 : 1) : 0
-    );
-
-    offsetX = slideNumber * sliderWidth;
-
-    slider.style['-webkit-transition-duration'] = '.2s';
-    slider.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
-
-    e = new CustomEvent('slide', {
-      detail: { slideNumber: Math.abs(slideNumber) },
-      bubbles: true,
-      cancelable: true
-    });
-
-    slider.parentNode.dispatchEvent(e);
-  };
-
-  window.Ratchet.Sliders = {
-    onTouchStart: onTouchStart,
-    onTouchMove: onTouchMove,
-    onTouchEnd: onTouchEnd
-  };
-
-}());
-(function() {
-  var __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
-  App.Widgets.Slider = (function(_super) {
-    __extends(Slider, _super);
-
-    function Slider() {
-      return Slider.__super__.constructor.apply(this, arguments);
-    }
-
-    Slider.prototype.events = {
-      "touchstart": "onTouchStart",
-      "touchmove": "onTouchMove",
-      "touchend": "onTouchEnd",
-      "mousedown": "onMouseDown",
-      "mousemove": "onMouseMove",
-      "mouseup": "onMouseUp"
-    };
-
-    Slider.prototype.onTouchStart = function(event) {
-      return Ratchet.Sliders.onTouchStart(event);
-    };
-
-    Slider.prototype.onTouchMove = function(event) {
-      return Ratchet.Sliders.onTouchMove(event);
-    };
-
-    Slider.prototype.onTouchEnd = function(event) {
-      return Ratchet.Sliders.onTouchEnd(event);
-    };
-
-    Slider.prototype._imitateTouchEvent = function(event) {
-      event.touches || (event.touches = [_.pick(event, "pageX", "pageY")]);
-      return event;
-    };
-
-    Slider.prototype.onMouseDown = function(event) {
-      this.mouseIsDown = true;
-      return this.onTouchStart(this._imitateTouchEvent(event));
-    };
-
-    Slider.prototype.onMouseMove = function(event) {
-      if (this.mouseIsDown) {
-        return this.onTouchMove(this._imitateTouchEvent(event));
-      }
-    };
-
-    Slider.prototype.onMouseUp = function(event) {
-      this.mouseIsDown = false;
-      return this.onTouchEnd(this._imitateTouchEvent(event));
-    };
-
-    return Slider;
 
   })(Backbone.View);
 
